55e24866c5ccc012bb8838b7c47f2a5409fe0dc6
praneeth199 <64678492+praneeth199@users.noreply.github.com>
2020-05-02 12:24:09 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+      double first, second;
+      printf("Enter first number: ");
+      scanf("%lf", &first);
+      printf("Enter second number: ");
+      scanf("%lf", &second);
+
+      
+
+      printf("\nAfter swapping, firstNumber = %.2lf\n",second);
+      printf("After swapping, secondNumber = %.2lf", first);
+      return 0;
+}
